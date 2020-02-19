8a8a6e0fb44ed271a206d3fbe7d536936f5ee6e1
GABRIEL AUGUSTO DOS SANTOS <19266840@CFP127ALUNO.LOCAL>
2020-02-19 11:27:24 -0300

    inicio do projeto

---
+++ header.php
@@ -0,0 +1,24 @@
+<nav>
+    <div class="container">
+        <figure id="logo">
+            <img src="https://prgbrasil.com/wp-content/themes/consultix/images/no-image-found-360x250.png">
+        </figure>
+        <ul id="containerMenu">
+            <li>Home</li>
+            <li>Curiosidades</li>
+            <li>Sobre a empresa</li>
+            <li>Promoções</li>
+            <li>Nossas Lojas</li>
+            <li>Produto do mês</li>
+            <li>Contato</li>
+        </ul>
+        <div id="containerLogin">
+            <div class="camposLogin">
+                
+            </div>
+            <div class="camposLogin">
+                
+            </div>
+        </div>
+    </div>
+</nav>
