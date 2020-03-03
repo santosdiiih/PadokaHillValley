dfc57b4250eec7e130032315090ce9c8bdcbd45e
GABRIEL AUGUSTO DOS SANTOS <19266840@CFP127ALUNO.LOCAL>
2020-03-03 11:25:26 -0300

    construindo a area dos produtos

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+    <div class="container">
+        <div id="botaoSistemaInterno">
+            <a href="#">Sistema Interno</a>
+        </div>
+        <div id="footerEndereco">
+            Endereço:
+            <a href="https://goo.gl/maps/pejwxxympYwoWM8M6" target="_blank">Rua Elton Silva, 905 - Centro, Jandira - SP, 06600-025</a>
+        </div>
+        <div id="containerAppDownload">
+            <figure id="iconeApp">
+                <img src="img/playstore.png" alt="PadokaHillAPP" title="aplicativo da padaria">
+            </figure>
+            <div id="botaoBaixarApp">
+                Baixe o App
+            </div>
+        </div>
+    </div>
+</footer>
--- index.php
+++ index.php
@@ -4,13 +4,18 @@
         <meta charset="utf-8">
         <title>Padoka Hill Valley</title>
         <link rel="stylesheet" type="text/css" href="css/style.css">
+        <link rel="stylesheet" type="text/css" href="css/padrao.css">
     </head>
     <body>
         <?php include_once('header.php')?>
         <div id="areaProdutos">
             <div class="container">
-                
+                <ul id="filtroDosProdutos">
+                    <li class="filtroOpcao formataMenu">Item 1</li>
+                    <li class="filtroOpcao formataMenu">Item 1</li>
+                </ul>
             </div>
         </div>
+        <?php include_once('footer.php')?>
     </body>
 </html>
