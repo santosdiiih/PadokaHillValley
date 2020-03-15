849bb6a478e6fdc0a9877015ad1c546680d06af0
augustos0204 <course.gabrielaug@gmail.com>
2020-03-15 04:12:25 -0300

    mapa quase finalizado, faltando apenas o redirecionamento das lojas

--- js/map.js
+++ js/map.js
@@ -1,9 +1,48 @@
-"use-strict"
+//Variavel que contem o visual do mapa
+const provedorDoMapa = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 
-function iniciarMap(){
-    let cord = {lat: -23.5283186,lng: -46.8979534};
-    const map = new google.maps.Map(document.getElementById('map'),{
-        zoom: 10,
-        center: cord
-    });
-}
+//Variavel que criei para guardar as coordenadas da minha residência
+const cordenadaInicial = [-23.54033, -46.96644];
+
+//Variável onde alocamos o mapa e definimos as coordenadas iniciais e seu zoom
+let mapaDaPagina = L.map('myMap').setView(cordenadaInicial, 13);
+
+//aidiconando a imagem do mapa no mapa
+L.tileLayer(provedorDoMapa, {
+    maxZoom: 18,
+}).addTo(mapaDaPagina);
+
+let iconeDoMarcador = L.icon({
+    iconUrl: 'img/pin.png',
+    iconSize: [60, 60],
+    iconAnchor: [30, 60],
+});
+
+//Removendo o efeito de adicionar zoom ao dar Double CLick no mapa
+mapaDaPagina.doubleClickZoom.disable();
+
+//adicionando um evento que ao dar double click o mapa se redireciona para determinada coordenada
+mapaDaPagina.addEventListener('dblclick', e => {
+    let coordenada = mapaDaPagina.mouseEventToLatLng(e.originalEvent);
+    L.marker([coordenada.lat, coordenada.lng], {icon: iconeDoMarcador}).addTo(mapaDaPagina);
+    //exibindo no console a coordenada
+    console.log(coordenada);
+});
+
+
+//função que recebe a localização do navegador
+navigator.geolocation.getCurrentPosition(
+    (pos) => {
+        const { coords } = pos;
+        console.log(coords);
+        L.marker([coords.latitude, coords.longitude]).addTo(mapaDaPagina);
+    },
+    (err) => {
+        console.log(err);
+    },
+    {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+    }
+)
--- nossas_lojas.php
+++ nossas_lojas.php
@@ -6,14 +6,14 @@
         <link rel="stylesheet" type="text/css" href="css/style.css">
         <link rel="stylesheet" type="text/css" href="css/padrao.css">
         <link rel="stylesheet" type="text/css" href="css/slide.css">
-        <link rel="stylesheet" type="text/css" href="css/map.css">
+        <link rel="stylesheet" href="leaflet/leaflet.css">
     </head>
     <body>
         <?php include_once('header.php')?>
         <div id="areaDoMapa">
             <div class="container">
-                <h1> Nossa loja principal está localizada em: </h1>
-                <div id="map">
+                <h1> Visualize as lojas no mapa: </h1>
+                <div id="myMap">
 
                 </div>
             </div>
@@ -22,7 +22,7 @@
         <script type="text/javascript" src="js/jquery.js"></script>
         <script type="text/javascript" src="js/cycle2.js"></script>
         <script type="text/javascript" src="js/slide.js"></script>
+        <script src="leaflet/leaflet.js" type="text/javascript"></script>
         <script type="text/javascript" src="js/map.js"></script>
-        <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDupWIQI34kN1Ob4ApcwWCYl0YFBY-mqEU&callback=iniciarMap" async defer></script>
     </body>
 </html>
